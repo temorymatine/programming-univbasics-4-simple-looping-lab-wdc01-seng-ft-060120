2a8f10c4038c7ca4672e9c27c70df9b976e8a1cc
Matine temory <temorymatine@gmail.com>
2020-05-16 17:04:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -19,9 +19,16 @@ end
   end
 end
    
+  
+  
+  def return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count] = array[count].to_s
+    count += 1
   end
-  
-  
+  array
+end
   
   
   
