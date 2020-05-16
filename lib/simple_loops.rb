d5b5f1de35eaf7b2bff42cf390a0ed7daf3feefa
Matine temory <temorymatine@gmail.com>
2020-05-16 17:02:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,28 @@
-# Write your methods here
+def loop_message_five_times(message)
+  message="Hello World."
+  5.times{puts message}
+end
+def loop_message_n_times(message,number)
+  message="Hello Moon."
+  number = 5
+  number.times{puts message}
+  message="Hello Red Balloon."
+  number =10 
+  number.times{puts message}
+end
+
+  def output_array (array,counter)
+    array =["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+    counter = 0
+   while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+   
+  end
+  
+  
+  
+  
+  
+  
