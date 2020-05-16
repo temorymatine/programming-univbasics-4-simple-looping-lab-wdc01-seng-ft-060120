645afecda4a86eb3f9dc62678e4493da09b1487b
Matine temory <temorymatine@gmail.com>
2020-05-16 17:05:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,7 +2,7 @@ def loop_message_five_times(message)
   message="Hello World."
   5.times{puts message}
 end
-def loop_message_n_times(message,number)
+def loop_message_n_times(message)
   message="Hello Moon."
   number = 5
   number.times{puts message}
